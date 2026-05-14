7a481867acd6ec5146b116b730850640c079d5b8
paulAlexSerban <paul.alex.serban@gmail.com>
2026-05-14 22:29:47 +0300

    feat(part1): add ex 1.7 sol

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,19 +1,37 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const all = good + neutral + bad;
+
+  const average = all === 0 ? 0 : (good - bad) / all;
+  const positive = all === 0 ? 0 : (good / all) * 100;
 
   return (
     <div>
       <div>
-      <button></button>
-      <button></button>
-      <button></button>
+        <h2>give feedback</h2>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+
+      <div>
+        <h2>statistics</h2>
+        <ul>
+          <li>good: {good}</li>
+          <li>neutral: {neutral}</li>
+          <li>bad: {bad}</li>
+          <li>all: {all}</li>
+          <li>average: {average}</li>
+          <li>positive: {positive} %</li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
