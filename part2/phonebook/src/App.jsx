ffbf18c23391257992974a57b5abc825a2cd55a9
paulAlexSerban <paul.alex.serban@gmail.com>
2026-05-15 09:47:26 +0300

    feat(part2): add ex 2.6 sol

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    const personObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
-}
 
-export default App
+      <div>debug: {newName}</div>
+
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
