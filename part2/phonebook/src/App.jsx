393ea2bd42a4430c5caf04b9e2dd2931e394b694
paulAlexSerban <paul.alex.serban@gmail.com>
2026-05-15 10:34:07 +0300

    feat(part2): add ex 2.11 sol

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,51 +1,54 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-import Filter from './components/filter'
-import PersonForm from './components/pearsonForm'
-import Persons from './components/persons'
+import Filter from "./components/filter";
+import PersonForm from "./components/pearsonForm";
+import Persons from "./components/persons";
+
+const API_ENDPOINT = "http://localhost:3001/persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filterQuery, setFilterQuery] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterQuery, setFilterQuery] = useState("");
 
-  const handleNameChange = (event) => setNewName(event.target.value)
-  const handleNumberChange = (event) => setNewNumber(event.target.value)
-  const handleFilterChange = (event) => setFilterQuery(event.target.value)
+  useEffect(() => {
+    axios.get(API_ENDPOINT).then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
+  const handleNameChange = (event) => setNewName(event.target.value);
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
+  const handleFilterChange = (event) => setFilterQuery(event.target.value);
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const nameExists = persons.some(
-      person => person.name.toLowerCase() === newName.trim().toLowerCase()
-    )
+      (person) => person.name.toLowerCase() === newName.trim().toLowerCase(),
+    );
 
     if (nameExists) {
-      alert(`${newName} is already added to phonebook`)
-      return
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
 
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1
-    }
+      id: String(persons.length + 1),
+    };
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-  }
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
 
-  const personsToShow = persons.filter(person =>
-    person.name.toLowerCase().includes(filterQuery.toLowerCase())
-  )
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(filterQuery.toLowerCase()),
+  );
 
   return (
     <div>
@@ -53,18 +56,18 @@ const App = () => {
       <Filter value={filterQuery} onChange={handleFilterChange} />
 
       <h3>Add a new</h3>
-      <PersonForm 
+      <PersonForm
         onSubmit={addPerson}
         nameValue={newName}
         onNameChange={handleNameChange}
         numberValue={newNumber}
         onNumberChange={handleNumberChange}
       />
-      
+
       <h3>Numbers</h3>
       <Persons personsToShow={personsToShow} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
