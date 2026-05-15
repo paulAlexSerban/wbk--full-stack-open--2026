e1ba7dddc085a948783bce88ccaac36dc9a1a614
paulAlexSerban <paul.alex.serban@gmail.com>
2026-05-15 19:31:09 +0300

    feat(part3): add ex 3.2 sol

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,32 +1,43 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
-const phonebookData = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+const persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
   },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ];
 
-app.get('/api/persons', (req, res) => {
-  res.json(phonebookData);
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  const requestTime = new Date();
+  const phonebookCount = persons.length;
+
+  const html = `
+    <p>Phonebook has info for ${phonebookCount} people</p>
+    <p>${requestTime}</p>
+  `;
+
+  res.send(html);
 });
 
 const PORT = 3001;
