aafe4173881b6ac9547dd2483f5fa940fd63a339
paulAlexSerban <paul.alex.serban@gmail.com>
2026-05-16 13:49:38 +0300

    feat(part3): sol 3.11 done

--- part3/phonebook-frontend/src/services/persons.js
+++ part3/phonebook-frontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || process.env.RENDER_EXTERNAL_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const BASE_URL = `${API_URL}/persons`
 
