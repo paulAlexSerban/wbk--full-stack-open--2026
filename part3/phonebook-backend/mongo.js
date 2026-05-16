b46358820518761552a12ac220282d7bb0c511ab
paulAlexSerban <paul.alex.serban@gmail.com>
2026-05-16 22:30:29 +0300

    feat(part3): sol 3.12 done

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,40 +1,58 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const dotenv = require('dotenv');
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>",
+  );
+  process.exit(1);
+}
 
 const MONGO_DB_USERBANE = process.env.MONGO_DB_USERBANE;
-const MONGODB_PASWORD = process.env.MONGODB_PASWORD;
+const MONGODB_PASWORD = process.argv[2];
 const MONGODB_ATLAS_CLUSTER = process.env.MONGODB_ATLAS_CLUSTER;
 const MONGODB_ATLAS_APP_NAME = process.env.MONGODB_ATLAS_APP_NAME;
 
+const DB_URL = `mongodb+srv://${MONGO_DB_USERBANE}:${MONGODB_PASWORD}@${MONGODB_ATLAS_CLUSTER}/?appName=${MONGODB_ATLAS_APP_NAME}&retryWrites=true&w=majority`;
 
-const DB_URL = `mongodb+srv://${MONGO_DB_USERBANE}:${MONGODB_PASWORD}@${MONGODB_ATLAS_CLUSTER}/?appName=${MONGODB_ATLAS_APP_NAME}`
+console.log({ DB_URL });
 
+mongoose.set("strictQuery", false);
+mongoose.connect(DB_URL, { family: 4 });
 
-mongoose.set('strictQuery',false)
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
 
-mongoose.connect(DB_URL, { family: 4 })
+const Person = mongoose.model("Person", personSchema);
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
-
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
-
-Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log({note})
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
     });
-})
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  console.log("Invalid arguments. Use one of the following formats:");
+  console.log("  To list entries: node mongo.js <password>");
+  console.log('  To add an entry: node mongo.js <password> "<name>" <number>');
+  mongoose.connection.close();
+}
