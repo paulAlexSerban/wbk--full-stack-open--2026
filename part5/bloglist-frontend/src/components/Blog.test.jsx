dc5e5ccccabc4d239e14ba002afbd432e966b219
paulAlexSerban <paul.alex.serban@gmail.com>
2026-05-21 14:37:29 +0300

    feat(part5): 5.14 done

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { describe, test, expect } from 'vitest'
 import Blog from './Blog'
 
@@ -26,5 +27,33 @@ describe('<Blog />', () => {
 
     expect(screen.queryByText('https://paulserban.eu/blog/blog_page')).toBeNull()
     expect(screen.queryByText(/likes/)).toBeNull()
+  })
+
+  test('renders URL and likes when the view button has been clicked', async () => {
+    const blog = {
+      title: 'Clicks can be tested in react with compoenent testing',
+      author: 'lePaul Tests All',
+      url: 'https://paulserban.eu/testing',
+      likes: 1,
+      user: {
+        username: 'lePaulQa',
+        name: 'Paul Serban QA'
+      }
+    }
+
+    const { container } = render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+
+    const button = screen.getByText('view')
+    await user.click(button)
+
+
+    const detailsDiv = container.querySelector('.blog__details')
+    expect(detailsDiv).not.toBeNull()
+
+    expect(detailsDiv).toHaveTextContent('https://paulserban.eu/testing')
+    expect(detailsDiv).toHaveTextContent('likes 1')
+    expect(detailsDiv).toHaveTextContent('Paul Serban QA')
   })
 })
