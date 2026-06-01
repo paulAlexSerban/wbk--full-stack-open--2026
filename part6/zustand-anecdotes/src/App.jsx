bd58a713ecdeb592c91f23e2a482de808c224c9c
paulAlexSerban <paul.alex.serban@gmail.com>
2026-06-01 17:34:34 +0300

    feat(part6): 6.7 done

--- part6/zustand-anecdotes/src/App.jsx
+++ part6/zustand-anecdotes/src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
+import anecdoteService from "./services/anecdotes";
+import { useAnecdotesActions } from "./store";
 
 const App = () => {
+  const { initializeAnecdotes } = useAnecdotesActions();
+
+  useEffect(() => {
+    anecdoteService.getAll().then((anecdotes) => initializeAnecdotes(anecdotes));
+  }, [initializeAnecdotes]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6/zustand-anecdotes/src/store.js
+++ part6/zustand-anecdotes/src/store.js
@@ -1,22 +1,10 @@
 import { create } from "zustand";
 
-const initialContent = [
-  "If it hurts, do it more often.",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-  "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-  "The only way to go fast, is to go well.",
-];
-
-const anecdotes = initialContent.map((content, id) => ({ content, id, votes: 0 }));
-
 const useAnecdotesStore = create((set) => ({
-  anecdotes,
+  anecdotes: [],
   filter: "",
   actions: {
+    initializeAnecdotes: (anecdotes) => set({ anecdotes }),
     setFilter: (filter) => set({ filter }),
     vote: (id) =>
       set((state) => ({
@@ -28,7 +16,9 @@ const useAnecdotesStore = create((set) => ({
       })),
     createAnecdote: (content) =>
       set((state) => {
-        const id = Math.max(...state.anecdotes.map((a) => a.id), -1) + 1;
+        const id = String(
+          Math.max(...state.anecdotes.map((a) => Number(a.id)), 0) + 1,
+        );
         return {
           anecdotes: state.anecdotes.concat({ content, id, votes: 0 }),
         };
